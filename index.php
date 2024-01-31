c3af1a8a52ded1be14ef00f7c0e3acba081f15ce
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-31 16:04:17 -0300

    configurações iniciais

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    include_once("helper/url.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Codar</title>
+    <link rel="icon" type="image/x-icon" href="img/logo.svg">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <h1>Olá Mundo!</h1>
+</body>
+</html>
