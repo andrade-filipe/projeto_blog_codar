4cc38b6858d319bb7c714c5c0044241eaafe092c
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-31 16:44:43 -0300

    finalizado os posts do blog com as tags

--- index.php
+++ index.php
@@ -13,6 +13,12 @@
                     <h2 class="post-title">
                         <a href="<?= $BASE_URL ?>post.php?id=<?= $post["id"]?>"><?= $post['title'] ?></a>
                     </h2>
+                    <p class="post-description"><?= $post['description'] ?></p>
+                    <div class="tags-container">
+                        <?php foreach($post['tags'] as $tag): ?>
+                            <a href="#"> <?= $tag ?> </a>
+                        <?php endforeach; ?>
+                    </div>
                 </div>
             <?php endforeach; ?>
         </div>
